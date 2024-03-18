860e948374d53f44b8a48d90b26c52bcde99ec8c
Ed1rac (Edkallenn Lima) <ed1rac@users.noreply.github.com>
2024-03-18 03:20:19 +0000

    alternado clang, dicionario, env variables e outros

--- c/estruturasdados/primeiro.c
+++ c/estruturasdados/primeiro.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
+#include <cs50.h>  //Obrigaório para get_string e o tipo de dados "string" que é um TAD
 
 int main(void){
 
+    string nome = get_string("Digite seu nome: ");
+
     printf("Olá, mundo!\n");
+    printf("Tudo bem, %s\n", nome);
     printf("Bem vindo ao Unipê!\n");
     return 1;
 
+    // para compilar este programa, digite no terminal:
+    // make primeiro
+    // Para executar, em seguida digite: ./primeiro
 }
